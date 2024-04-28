68442e466c78f0d4a2e79996c1a8bb151588d207
Gorthdzx <krislaksana123@gmail.com>
2024-04-28 10:49:36 +0700

    refactor : error handler using dictionary

--- Cogs/ErrorHandler.py
+++ Cogs/ErrorHandler.py
@@ -5,23 +5,20 @@ import discord
 class HandleError(commands.Cog):
     def __init__(self, bot: discord.Client):
         self.bot = bot
+        self.error_messages = {
+            commands.MemberNotFound: "Member not found !!",
+            commands.MissingRequiredArgument: "Missing parameter Input !!!",
+            commands.CommandInvokeError: "Object has no attribute send",
+            commands.NotOwner: "You are not owner of this bot !!!",
+            commands.UserInputError: "Input error !!",
+            commands.CommandNotFound: "Command not found !!",
+            commands.CommandOnCooldown: "Command on cooldown !!"
+        }
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.MemberNotFound):
-            await ctx.send("Member not found !!")
-
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Missing parameter Input !!!")
-
-        if isinstance(error, commands.CommandInvokeError):
-            await ctx.send("Object has no attribute send")
-
-        if isinstance(error, commands.NotOwner):
-            await ctx.send("You are not owner of this bot !!!")
-
-        if isinstance(error, commands.UserInputError):
-            await ctx.send("Input error !!")
+        if error.__class__ in self.error_messages:
+            await ctx.send(self.error_messages[error.__class__])
 
 
 async def setup(bot: commands.Bot):
